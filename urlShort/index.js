6c8819300e14e0113c4038749ce67e35790a3f26
lkgthub001 <159637960+lkgthub001@users.noreply.github.com>
2024-02-10 21:35:12 +0530

    Update index.js

--- urlShort/index.js
+++ urlShort/index.js
@@ -12,8 +12,8 @@ const userRoute = require('./routes/user');
 const {restrictToLoggedinUserOnly, checkAuth} = require('./middlewares/auth')
 const app = express();
 const PORT = 8001;
-const MONGODB_URI = "mongodb://127.0.0.1:27017/short-url"
-// const MONGODB_URI =process.env.DB_URI;
+// const MONGODB_URI = "mongodb://127.0.0.1:27017/short-url"
+const MONGODB_URI =process.env.DB_URI;
 
 connectToMongoDB(MONGODB_URI).then(()=>{
     console.log("Connected to MongoDB");
@@ -45,42 +45,3 @@ app.listen(PORT, ()=>{
 
 
 
-// const express = require('express');
-// const urlRoute = require('./routes/url');
-// const {connectToMongoDB} = require('./connect');
-// const URL = require('./models/url');
-// const shortid = require('shortid');
-
-// const app = express();
-// const PORT = 8001;
-
-// connectToMongoDB('mongodb://127.0.0.1:27017/short-url').then(()=>{
-//     console.log("Connected to MongoDB");
-// }).catch((err)=>{
-//     console.log("Error occured", err);
-// })
-
-// app.use(express.json());
-
-
-// app.use('/url', urlRoute);
-// app.get('/:shortid',async (req, res)=>{
-//     const shortID = req.params.shortid;
-//     const r = await URL.findOneAndUpdate({
-//         shortID,
-//     }, {
-//         $push: {
-//             visitHistory : {
-//                 timestamp: Date.now(),
-//             },
-//         }
-//     })
-    
-//     const entry = await URL.findOne({shortId : shortID})
-//     console.log(entry);
-//     res.redirect(entry.redirectURL);
-    
-// })
-// app.listen(PORT, ()=>{
-//     console.log(`Server started at PORT ${PORT}`);
-// })
